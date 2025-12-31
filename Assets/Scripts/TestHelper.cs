351b59b3447baeb4c7961e2415027c34c01fd895
강언덕 <64720058+123456789qwaszx@users.noreply.github.com>
2025-12-31 12:00:42 +0900

    test: implement clicker UI

---
+++ Assets/Scripts/TestHelper.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TestHelper : MonoBehaviour
+{
+    private UIBootStrap _bootstrap;
+    private UIOpener _uiOpener;
+    
+    void Start()
+    {
+        if (_bootstrap == null)
+            _bootstrap = FindFirstObjectByType<UIBootStrap>();
+        
+        _uiOpener = _bootstrap.Opener;
+    }
+    
+    public void OnOpenTitle()
+    {
+        _uiOpener.Open(UIActionKeys.OpenClickerTitle);
+    }
+}
