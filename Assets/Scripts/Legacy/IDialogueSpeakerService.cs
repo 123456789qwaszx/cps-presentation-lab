417f11713f8ae10821212bd7de9063252130f52b
강언덕 <64720058+123456789qwaszx@users.noreply.github.com>
2026-01-12 23:58:48 +0900

    chore: remove unused classes

--- Assets/Scripts/Legacy/IDialogueSpeakerService.cs
+++
@@ -1,7 +0,0 @@
-// using UnityEngine;
-//
-// public interface IDialogueSpeakerService
-// {
-//     string GetDisplayName(string speakerId);
-//     Sprite GetPortrait(string speakerId, Expression expression);
-// }
